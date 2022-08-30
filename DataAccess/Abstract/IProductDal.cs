a4d1e2443ef65b8f863a7a162dac8ae9f864e9ab
isocodes51 <flyingcow51@hotmail.com>
2022-08-31 00:24:25 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using Entitites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ Entitites/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entitites.Abstract
+{
+    //IEntity implement eden Class bir VeriTabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
