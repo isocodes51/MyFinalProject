9aac61e4824da96e3e71b4f41b1c8f4a87997e68
isocodes51 <flyingcow51@hotmail.com>
2022-08-31 23:00:34 +0300

    31.08.2022

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entitites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            //Yetkisi Var mı 
+            //Geçtiyse - Buralarda if ile yazılmış kodlar var.
+            return _productDal.GetAll();
+        }
+    }
+   
+}
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -2,30 +2,69 @@
 using Entitites.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {    //Oracle, Sql Server, PostgreSql, MongoDb den geliyormuş gibi (Fake) 
+            _products = new List<Product> {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryId=1,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+            };
+        }
+        
         public void Add(Product product)
         {
-            throw new NotImplementedException();
+            _products.Add(product); //Buradaki Add List to Add yani List in kendi fonksiyonu olan Add, yukarıdaki bizim fonksiyonun Add i değil 
         }
 
         public void Delete(Product product)
         {
-            throw new NotImplementedException();
+            //LINQ kullanmadan
+            //Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+            //----------------------
+
+            //LINQ kullanınca 
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            //--------------
+
+            _products.Remove(productToDelete);
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _products; //Tüm veri tabanını döndürüyoruz.
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return  _products.Where(p => p.CategoryId == categoryId).ToList();
         }
 
         public void Update(Product product)
         {
-            throw new NotImplementedException();
+            //Gönderdiğim ürün id sine sahip olan listedeki ürünü bul.
+            Product productToUpdate= _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            //Listedeki referansın birim fiyatını gönderidiğim product ın unitPrice yapıyoruz.
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
         }
     }
 }
