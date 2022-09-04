9c87924513a9bc81c89e58c341e747e66ad57f1c
isocodes51 <flyingcow51@hotmail.com>
2022-09-04 21:54:43 +0300

    .

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entitites.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{ //NuGet
+   public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            //IDisposible pattern implement of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity); //Referansı yakalama
+                addedEntity.State = EntityState.Added; //Eklenecek nesne
+                context.SaveChanges(); //Ekle ve Kaydet
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity); //Referansı yakalama
+                deletedEntity.State = EntityState.Deleted; //Silinecek nesne
+                context.SaveChanges(); //Ekle ve Kaydet
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity); //Referansı yakalama
+                updatedEntity.State = EntityState.Modified; //Güncellenecek nesne
+                context.SaveChanges(); //Ekle ve Kaydet
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entitites.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{   //Context: Vt tabloları ile Proje Class larını ilişkilendirmek
+    public class NorthwindContext : DbContext //Hangi Vt'ye bağlanacak
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //proje hangi vt ile ilişkili olduğunu belirttiğimiz yer
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");//Connetcion string yazıyoruz
+        }
+        public DbSet<Product> Products { get; set; } //Hangi Nesne hangi vt nesnesine karşılık gelecek
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
